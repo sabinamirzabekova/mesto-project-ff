d3c881ea3401918b017d99a4947b96b2c8587f32
Sabina Mirzabekova <binasmiras@gmail.com>
2024-09-12 19:20:33 +0300

    help

---
+++ components/cards.js
@@ -0,0 +1,10 @@
+
+import card_1 from '../images/card_1.jpg';
+import card_2 from '../images/card_2.jpg';
+import card_3 from '../images/card_3.jpg';
+
+export const initialCards = [
+  {name: "Card_1", link: card_1, owner: {_id: "-1"}},
+  {name: "Card_2", link: card_2, owner: {_id: "-1"}},
+  {name: "Card_3", link: card_3, owner: {_id: "-1"}}
+]
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import './index.css';
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+// @todo: DOM узлы
+const cardContainer = document.querySelector('.places__list')
+// @todo: Функция создания карточки
+function createCard(place, deleteCard){
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    const image = cardElement.querySelector('.card__image');
+    image.src = place.link;
+    image.alt = place.name;
+    cardElement.querySelector('.card__title').textContent = place.name;
+    
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+}
+// @todo: Функция уда
+function deleteCard(item){
+    item.target.closest('.places__item').remove();
+}
+// @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    const itemCard = createCard(item, deleteCard);
+    cardContainer.append(itemCard);
+});
+
